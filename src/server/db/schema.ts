bf7acd1b9aa8fe6ed46743eab54fc6ff32d0f5ca
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-02-23 07:07:36 -0600

    add db tables: users, posts, comments

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,12 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
+  type AnyPgColumn
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,17 +19,31 @@ import {
  */
 export const createTable = pgTableCreator((name) => `cs-reddit-clone_${name}`);
 
+export const users = createTable("users", {
+  id: serial("id").primaryKey(),
+  username: varchar("username").notNull(),
+  avatarUrl: varchar("avatar_url").default('/images/person.png'),
+});
+
 export const posts = createTable(
   "post",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    title: varchar("name", { length: 256 }).notNull(),
+    body: varchar("name", { length: 256 }).notNull(),
+    authorId: integer("author_id").references(() => users.id).notNull(),
+    voteCount: integer("vote_count").default(0).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updatedAt"),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
 );
+
+export const comments = createTable("comments", {
+  id: serial("id").primaryKey(),
+  body: varchar("body").notNull(),
+  authorId: integer("author_id").references(() => users.id).notNull(),
+  postId: integer("post_id").references(() => posts.id).notNull(),
+  parentId: integer("parent_id").references((): AnyPgColumn => comments.id).notNull(),
+});
