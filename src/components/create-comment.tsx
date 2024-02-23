d3d22c1d4e8224fbb9de22ab131fb1f1d197d67e
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-02-23 17:52:27 -0600

    add user session ternary to create-posts and create-comments

--- src/components/create-comment.tsx
+++ src/components/create-comment.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 
 import { api } from "~/trpc/react";
 import { Button } from "~/components/ui/button";
-import Image from "next/image";
 
 interface CreatePostProps {
   postId: number;
@@ -13,6 +14,7 @@ interface CreatePostProps {
 }
 
 export function CreateComment(props: CreatePostProps) {
+  const user = useUser();
   const { postId, authorId } = props;
   const router = useRouter();
   const [body, setBody] = useState("");
@@ -25,41 +27,45 @@ export function CreateComment(props: CreatePostProps) {
   });
 
   return (
-    <div className="shadow-input flex w-full flex-row items-start justify-around rounded-xl border border-gray-200 p-4 text-base">
-      <span className="pr-4">
-        <Image
-          src={"/images/person.png"}
-          alt={"Avatar"}
-          className="rounded-full"
-          width={24}
-          height={24}
-        />
-      </span>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createPost.mutate({ body });
-        }}
-        className="flex w-full flex-col gap-2 "
-      >
-        <input
-          type="text"
-          placeholder="Comment your thoughts"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          className="w-full text-black"
-        />
+    <>
+      {!!user.isSignedIn && (
+        <div className="shadow-input flex w-full flex-row items-start justify-around rounded-xl border border-gray-200 p-4 text-base">
+          <span className="pr-4">
+            <Image
+              src={"/images/person.png"}
+              alt={"Avatar"}
+              className="rounded-full"
+              width={24}
+              height={24}
+            />
+          </span>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              createPost.mutate({ body });
+            }}
+            className="flex w-full flex-col gap-2 "
+          >
+            <input
+              type="text"
+              placeholder="Comment your thoughts"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              className="w-full text-black"
+            />
 
-        <hr className="mb-2 w-full text-gray-200" />
+            <hr className="mb-2 w-full text-gray-200" />
 
-        <Button
-          type="submit"
-          className="h-9 w-24 self-end rounded-lg bg-indigo-500 px-3 py-2 text-sm font-semibold text-white transition"
-          disabled={createPost.isLoading}
-        >
-          {createPost.isLoading ? "Commenting..." : "Comment"}
-        </Button>
-      </form>
-    </div>
+            <Button
+              type="submit"
+              className="h-9 w-24 self-end rounded-lg bg-indigo-500 px-3 py-2 text-sm font-semibold text-white transition"
+              disabled={createPost.isLoading}
+            >
+              {createPost.isLoading ? "Commenting..." : "Comment"}
+            </Button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/create-post.tsx
+++ src/components/create-post.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 
 import { api } from "~/trpc/react";
 import { Button } from "~/components/ui/button";
-import Image from "next/image";
 
 export function CreatePost() {
+  const user = useUser();
   const router = useRouter();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -21,49 +23,53 @@ export function CreatePost() {
   });
 
   return (
-    <div className="shadow-input flex w-full flex-row items-start justify-around rounded-xl border border-gray-200 p-4 text-base">
-      <span className="pr-4">
-        <Image
-          src={"/images/person.png"}
-          alt={"Avatar"}
-          className="rounded-full"
-          width={24}
-          height={24}
-        />
-      </span>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createPost.mutate({ title, body });
-        }}
-        className="flex w-full flex-col gap-2 "
-      >
-        <input
-          type="text"
-          placeholder="Title of your post"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-full text-black"
-        />
+    <>
+      {!!user.isSignedIn && (
+        <div className="shadow-input flex w-full flex-row items-start justify-around rounded-xl border border-gray-200 p-4 text-base">
+          <span className="pr-4">
+            <Image
+              src={"/images/person.png"}
+              alt={"Avatar"}
+              className="rounded-full"
+              width={24}
+              height={24}
+            />
+          </span>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              createPost.mutate({ title, body });
+            }}
+            className="flex w-full flex-col gap-2 "
+          >
+            <input
+              type="text"
+              placeholder="Title of your post"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="w-full text-black"
+            />
 
-        <input
-          type="text"
-          placeholder="Share your thoughts with the world!"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-          className="w-full text-black"
-        />
+            <input
+              type="text"
+              placeholder="Share your thoughts with the world!"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              className="w-full text-black"
+            />
 
-        <hr className="mb-2 w-full text-gray-200" />
+            <hr className="mb-2 w-full text-gray-200" />
 
-        <Button
-          type="submit"
-          className="h-9 w-14 self-end rounded-lg bg-indigo-500 px-3 py-2 text-sm font-semibold text-white transition"
-          disabled={createPost.isLoading}
-        >
-          {createPost.isLoading ? "Posting..." : "Post"}
-        </Button>
-      </form>
-    </div>
+            <Button
+              type="submit"
+              className="h-9 w-14 self-end rounded-lg bg-indigo-500 px-3 py-2 text-sm font-semibold text-white transition"
+              disabled={createPost.isLoading}
+            >
+              {createPost.isLoading ? "Posting..." : "Post"}
+            </Button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
